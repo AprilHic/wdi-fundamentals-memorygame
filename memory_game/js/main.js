b21fb521caa5b2d7383a49466be3a5aceb156e6d
MacBook Pro <MacBookPro@aprilnotshowers.attlocal.net>
2019-09-14 17:25:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,35 @@ const cards = ["queen", "queen", "king", "king" ];
   
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-  cardsInPlay.push(cardOne);
-  console.log("User flipped" + cardOne);
+function checkForMatch() {
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again."); 
+  }       
+}
 
-var cardTwo = cards[3];
-  cardsInPlay.push(cardTwo);
-  console.log("User flipped" + cardTwo);
+function flipCard(cardId) {
+  if (cardsInPlay.length === 2); {
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]);
+    checkForMatch(); 
+ }
 
-if (cardsInPlay.length === 2); 
-    if (cardsInPlay[0] === cardsInPlay[3]) {
-  	 alert("You found a match!");
-} else {
-  	 alert("Sorry, try again.");
-}  	 	
+}
+flipCard([0]);
+flipCard([2]);
+
+
+
+     
+
+
+
+
+
+
+
   	
 
   	 
